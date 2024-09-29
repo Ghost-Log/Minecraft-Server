ad2d4d6e7516f6e9819cdcde99a4d4e6de3fdf3e
Ghost-Log <119308577+Ghost-Log@users.noreply.github.com>
2024-09-29 01:52:01 +0000

    Mundo guardado 2024-09-29

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ if not os.path.exists("./.gitignore"):
 	dec = base64.standard_b64decode(big).decode()
 	with open(".gitignore", 'w') as giti:
 		giti.write(dec)
-def download_latest_release(download_path='.'):
+def download_latest_release(download_path='.'):sddsd
 	mirror = "https://elyxdev.github.io/latest"
 	pet = requests.get(mirror)
 	if pet.status_code == 200:
